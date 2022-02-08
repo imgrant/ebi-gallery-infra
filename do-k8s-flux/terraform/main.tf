57eb6071fc41d4ef0e062a8b3b7ebd46e861fce1
Ian Grant <ian@iangrant.me>
2022-02-08 10:26:48 +0000

    Create a managed k8s cluster on DigitalOcean

--- do-k8s-flux/terraform/main.tf
+++ do-k8s-flux/terraform/main.tf
@@ -6,9 +6,35 @@ provider "digitalocean" {
   token = var.do_token
 }
 
+locals {
+  k8s_cluster_name  = "k8s-${lower(digitalocean_project.active.name)}"
+  db_cluster_name   = "db-${lower(digitalocean_project.active.name)}"
+  database = {
+    engine  = "mysql"
+    version = "8"
+  }
+}
+
 resource "digitalocean_project" "active" {
   name = var.do_project_name
   description = "EBI01948 technical challenge"
   environment = "Staging"
   resources = []
+}
+
+data "digitalocean_kubernetes_cluster" "primary" {
+  name        = module.doks-cluster.cluster_name
+  depends_on  = [module.doks-cluster]
+}
+
+module "doks-cluster" {
+  source                = "./doks-cluster"
+  cluster_name          = local.k8s_cluster_name
+  cluster_region        = var.do_region
+  cluster_version       = var.doks_cluster_version
+  cluster_upgrade_day   = var.doks_cluster_auto_upgrade_day
+  cluster_upgrade_time  = var.doks_cluster_auto_upgrade_time
+  worker_size           = var.doks_node_size
+  worker_count_min      = var.doks_nodes_min
+  worker_count_max      = var.doks_nodes_max
 }
--- do-k8s-flux/terraform/variables.tf
+++ do-k8s-flux/terraform/variables.tf
@@ -9,6 +9,47 @@ variable "do_project_name" {
   type = string
   default = "ebi-gallery"
 }
+
+variable "do_region" {
+  description = "DigitalOcean datacentre region to deploy in"
+  type = string
+  default = "lon1"
+}
+
+variable "doks_cluster_version" {
+  description = "Kubernetes version to use for DOKS cluster"
+  default = "1.21"
+}
+
+variable "doks_cluster_auto_upgrade_day" {
+  description = "Day of the week to perform cluster maintenance"
+  type = string
+  default = "sunday"
+}
+
+variable "doks_cluster_auto_upgrade_time" {
+  description = "Start time (in UTC) for the maintenance window"
+  type = string
+  default = "04:00"
+}
+
+variable "doks_nodes_min" {
+  description = "Minimum number of autoscaling nodes to deploy for DOKS cluster"
+  type = number
+  default = 1
+}
+
+variable "doks_nodes_max" {
+  description = "Maximum number of autoscaling nodes to deploy for DOKS cluster"
+  type = number
+  default = 3
+}
+
+variable "doks_node_size" {
+  description = "Size of droplets in the DOKS cluster node pool (use DO slug names)"
+  type = string
+  default = "s-1vcpu-2gb"
+}
 variable "github_owner" {
   description = "User or organization owning the GitHub repository for Flux"
   type = string
